704da4cd537d73c39b0a88861521dc20ee0d9a7f
Craftplacer <craftplacer@craftplacer.moe>
2023-03-27 00:15:23 +0200

    fix(misskey): map object emojis field variant to old schema

--- lib/src/misskey_emojis_conversion.dart
+++ lib/src/misskey_emojis_conversion.dart
@@ -1,5 +1,17 @@
+import 'package:fediverse_objects/src/misskey/emoji.dart';
+
 Object? misskeyEmojisReadValue(Map map, String key) {
   final value = map[key];
-  if (value is Map && value.isEmpty) return null;
+
+  if (value is Map) {
+    return [
+      for (final entry in value.entries)
+        Emoji(
+          name: entry.key,
+          url: entry.value,
+        ).toJson()
+    ];
+  }
+
   return value;
 }
