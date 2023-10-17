23490a12873d1a8ab41584d4d3faf292272b7d36
Craftplacer <craftplacer@waifu.club>
2023-10-17 03:29:54 +0200

    fix(misskey): expose `FollowingRequest`

--- lib/misskey.dart
+++ lib/misskey.dart
@@ -4,6 +4,7 @@ export '/src/misskey/channel.dart';
 export '/src/misskey/drive_file.dart';
 export '/src/misskey/emoji.dart';
 export '/src/misskey/error.dart';
+export '/src/misskey/following_request.dart';
 export '/src/misskey/manual/ad.dart';
 export '/src/misskey/manual/field.dart';
 export '/src/misskey/me_detailed.dart';
