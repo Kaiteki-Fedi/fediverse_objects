e0f1a9100747379e9f1fe98bfe74f1f14fb7aa75
Craftplacer <craftplacer@waifu.club>
2023-10-23 05:02:25 +0200

    fix(mastodon): remove foreign fields from `Role`

--- lib/src/mastodon/role.dart
+++ lib/src/mastodon/role.dart
@@ -7,21 +7,15 @@ class Role {
   final int id;
   final String name;
   final String color;
-  final int position;
   final int permissions;
   final bool highlighted;
-  final DateTime createdAt;
-  final DateTime updatedAt;
 
   const Role({
     required this.id,
     required this.name,
     required this.color,
-    required this.position,
     required this.permissions,
     required this.highlighted,
-    required this.createdAt,
-    required this.updatedAt,
   });
 
   factory Role.fromJson(Map<String, dynamic> json) => _$RoleFromJson(json);
--- lib/src/mastodon/role.g.dart
+++ lib/src/mastodon/role.g.dart
@@ -14,26 +14,17 @@ Role _$RoleFromJson(Map<String, dynamic> json) => $checkedCreate(
           id: $checkedConvert('id', (v) => v as int),
           name: $checkedConvert('name', (v) => v as String),
           color: $checkedConvert('color', (v) => v as String),
-          position: $checkedConvert('position', (v) => v as int),
           permissions: $checkedConvert('permissions', (v) => v as int),
           highlighted: $checkedConvert('highlighted', (v) => v as bool),
-          createdAt:
-              $checkedConvert('created_at', (v) => DateTime.parse(v as String)),
-          updatedAt:
-              $checkedConvert('updated_at', (v) => DateTime.parse(v as String)),
         );
         return val;
       },
-      fieldKeyMap: const {'createdAt': 'created_at', 'updatedAt': 'updated_at'},
     );
 
 Map<String, dynamic> _$RoleToJson(Role instance) => <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
       'color': instance.color,
-      'position': instance.position,
       'permissions': instance.permissions,
       'highlighted': instance.highlighted,
-      'created_at': instance.createdAt.toIso8601String(),
-      'updated_at': instance.updatedAt.toIso8601String(),
     };
